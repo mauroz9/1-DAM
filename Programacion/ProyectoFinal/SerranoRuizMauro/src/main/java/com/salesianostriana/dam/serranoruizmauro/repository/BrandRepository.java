a905def887326b13b79b78d4b5637dd53787f6d4
unknown <mauro.serranoo26@gmail.com>
2025-05-19 13:55:05 +0200

    terminado el apartado estatico de contacto,politica etc

--- Programacion/ProyectoFinal/SerranoRuizMauro/src/main/java/com/salesianostriana/dam/serranoruizmauro/repository/BrandRepository.java
+++
@@ -1,8 +0,0 @@
-package com.salesianostriana.dam.serranoruizmauro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.salesianostriana.dam.serranoruizmauro.model.Brand;
-
-public interface BrandRepository extends JpaRepository<Brand, Long>{
-
-}
--- Programacion/ProyectoFinal/SerranoRuizMauro/src/main/java/com/salesianostriana/dam/serranoruizmauro/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.salesianostriana.dam.serranoruizmauro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.salesianostriana.dam.serranoruizmauro.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-
-}
